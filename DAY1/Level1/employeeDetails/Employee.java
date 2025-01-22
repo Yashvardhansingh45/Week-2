2fecf8af8336fbaabf4c5bea8c2f22acf5d24c36
Yash vardhan Singh <yashvardhansinghparihar10@gmail.com>
2025-01-22 13:19:20 +0530

    Employee

--- DAY1/Level1/employeeDetails/Employee.java
+++ DAY1/Level1/employeeDetails/Employee.java
@@ -1,3 +1,19 @@
-class Employee{
-    
+
+public class Employee{
+    // Attributes
+    private String name;
+    private int id;
+    private double salary;
+    // Constructor
+    public Employee(String name, int id, double salary){
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    // method to display the details
+    public void displayDetails() {
+        System.out.println("Name of the Employee: " + name);
+        System.out.println("ID of the Employee: "+ id);
+        System.out.println("Salary of the Employee: "+ salary);
+    }
 }
---
+++ DAY1/Level1/employeeDetails/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String []args){
+        // create an object
+        Employee Employe1 = new Employee("Yash", 21, 10000000);
+        // call the displayDetails method
+        Employe1.displayDetails();
+    }
+}
